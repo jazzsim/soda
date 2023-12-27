c80421d1211aa72cca12e83036804ea0a77d522d
Jazz <jazz@info-in.net>
2023-12-27 18:18:16 +0800

    Fix showPassword issue

--- lib/pages/mobile/add_server.m.dart
+++ lib/pages/mobile/add_server.m.dart
@@ -9,93 +9,98 @@ import 'package:soda/widgets/extensions/padding.dart';
 import '../../widgets/components/dialogs/toast_overlay.dart';
 
 void addServerModal(WidgetRef ref, BuildContext context) {
-  final showPasswordStateProvider = StateProvider<bool>((ref) => true);
+  final showPasswordStateProvider = StateProvider<bool>((ref) => false);
 
   showModalBottomSheet(
     context: context,
     showDragHandle: true,
     useSafeArea: true,
     isScrollControlled: true,
-    builder: (BuildContext context) {
-      return AnimatedPadding(
-        duration: const Duration(milliseconds: 150),
-        curve: Curves.easeOut,
-        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Add new server',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Server url', border: OutlineInputBorder()),
-              keyboardType: TextInputType.url,
-              onChanged: (url) => ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(url: url)),
-            ).pb(20).pt(20),
-            ExpansionTile(
-              title: const Text(
-                'Credentials (Optional)',
+    builder: (BuildContext innerContext) {
+      return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+        return AnimatedPadding(
+          duration: const Duration(milliseconds: 150),
+          curve: Curves.easeOut,
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Add new server',
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-              children: [
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Username', border: OutlineInputBorder()),
-                  onChanged: (username) => ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(username: username)),
-                ).pb(5).pt(10),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        ref.watch(showPasswordStateProvider) ? Icons.visibility : Icons.visibility_off,
-                      ),
-                      onPressed: () => ref.read(showPasswordStateProvider.notifier).update((state) => !state),
-                    ),
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: ref.watch(showPasswordStateProvider),
-                  onChanged: (password) => ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(password: password)),
-                ).pb(10).pt(5),
-              ],
-            ).pb(20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CTextButton(
-                  'Cancel',
-                  onPressed: () {
-                    Navigator.pop(context); // Closes the dialog
-                  },
+              TextField(
+                decoration: const InputDecoration(labelText: 'Server url', border: OutlineInputBorder()),
+                keyboardType: TextInputType.url,
+                onChanged: (url) => ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(url: url)),
+              ).pb(20).pt(20),
+              ExpansionTile(
+                title: const Text(
+                  'Credentials (Optional)',
                 ),
-                PrimaryButton(
-                  'Add',
-                  onPressed: () {
-                    ref.invalidate(pathStateProvider);
-                    LoadingScreen(context).show();
-                    ref.read(contentControllerProvider).getPageContent().then((_) async {
-                      showToast(context, ToastType.success, 'Success');
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(labelText: 'Username', border: OutlineInputBorder()),
+                    onChanged: (username) => ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(username: username)),
+                  ).pb(5).pt(10),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: const OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                          icon: Icon(
+                            ref.watch(showPasswordStateProvider) ? Icons.visibility : Icons.visibility_off,
+                          ),
+                          onPressed: () => setState(
+                                () {
+                                  ref.read(showPasswordStateProvider.notifier).update((state) => !state);
+                                },
+                              )),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: !ref.watch(showPasswordStateProvider),
+                    onChanged: (password) => ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(password: password)),
+                  ).pb(10).pt(5),
+                ],
+              ).pb(20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CTextButton(
+                    'Cancel',
+                    onPressed: () {
+                      Navigator.pop(context); // Closes the dialog
+                    },
+                  ),
+                  PrimaryButton(
+                    'Add',
+                    onPressed: () {
+                      ref.invalidate(pathStateProvider);
+                      LoadingScreen(context).show();
+                      ref.read(contentControllerProvider).getPageContent().then((_) async {
+                        showToast(context, ToastType.success, 'Success');
 
-                      Navigator.of(context).pop();
-                      // save server details into shared preferences
-                      ref.read(contentControllerProvider).updateServerList();
-                      ref.invalidate(serverListStateProvider);
+                        Navigator.of(context).pop();
+                        // save server details into shared preferences
+                        ref.read(contentControllerProvider).updateServerList();
+                        ref.invalidate(serverListStateProvider);
 
-                      Uri serverUri = Uri.parse(ref.read(httpServerStateProvider).url);
-                      ref.read(pathStateProvider.notifier).state = serverUri.path;
-                      ref.read(titleStateProvider.notifier).state = serverUri.pathSegments.last;
-                      ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(url: serverUri.origin));
-                    }).catchError((err, st) {
-                      showToast(context, ToastType.error, err);
-                    });
-                  },
-                )
-              ],
-            )
-          ],
-        ).pa(20),
-      );
+                        Uri serverUri = Uri.parse(ref.read(httpServerStateProvider).url);
+                        ref.read(pathStateProvider.notifier).state = serverUri.path;
+                        ref.read(titleStateProvider.notifier).state = serverUri.pathSegments.last;
+                        ref.read(httpServerStateProvider.notifier).update((state) => state.copyWith(url: serverUri.origin));
+                      }).catchError((err, st) {
+                        showToast(context, ToastType.error, err);
+                      });
+                    },
+                  )
+                ],
+              )
+            ],
+          ).pa(20),
+        );
+      });
     },
   );
 }
