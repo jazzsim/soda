0f2de6c787ef85f26d146bec93b00854fccfd8d7
Jazz <jazz@info-in.net>
2024-05-15 21:31:49 +0800

    Fix EndDrawer in fullscreen

--- lib/widgets/components/video/video_control.dart
+++ lib/widgets/components/video/video_control.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:developer';
 
+import 'package:anydrawer/anydrawer.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,6 +23,10 @@ final showVideoControlProvider = StateProvider<bool>((ref) => false);
 final timerProvider = StateProvider<Timer?>((ref) => null);
 
 final durationProvider = StateProvider<Duration>((ref) => const Duration(milliseconds: 850));
+
+final endDrawerWidthProvider = StateProvider<bool>((ref) => false);
+
+final endDrawerVisibilityProvider = StateProvider<bool>((ref) => false);
 
 class VideoControlWidget extends ConsumerStatefulWidget {
   final Player player;
@@ -55,202 +61,192 @@ class _VideoControlWidgetState extends ConsumerState<VideoControlWidget> {
         ref.read(timerProvider)?.cancel();
         ref.read(videoTimerProvider)?.cancel();
       },
-      child: Listener(
-        onPointerDown: (event) {
-          windowManager.startDragging();
-        },
-        onPointerSignal: (pointerSignal) {
-          if (pointerSignal is PointerScrollEvent) {
-            // do something when scrolled
-            double currentVolume = widget.player.state.volume;
-            if (pointerSignal.scrollDelta.dy < 0) {
-              if (currentVolume + 2.0 > 100) {
-                widget.player.setVolume(100);
-              } else {
-                widget.player.setVolume(currentVolume + 2.0);
-              }
-            } else {
-              if (currentVolume - 2.0 < 0) {
-                widget.player.setVolume(0);
-              } else {
-                widget.player.setVolume(currentVolume - 2.0);
-              }
-            }
-            ref.read(volumeStateProvider.notifier).update((state) => widget.player.state.volume);
-            ref.read(showVolumeProvider.notifier).update((state) => true);
-          }
-        },
-        onPointerHover: (event) {
-          ref.read(showVideoControlProvider.notifier).update((state) => true);
-          setState(() {});
-          ref.watch(timerProvider)?.cancel();
-          ref.read(timerProvider.notifier).update(
-                (state) => Timer(
-                  ref.read(durationProvider),
-                  () {
-                    ref.read(showVideoControlProvider.notifier).update((state) => false);
-                    setState(() {});
+      child: CallbackShortcuts(
+        bindings: getShortcuts(widget.state, context, ref, widget.player),
+        child: Stack(
+          children: [
+            MouseRegion(
+              cursor: ref.watch(showVideoControlProvider) ? MouseCursor.defer : SystemMouseCursors.none,
+              onEnter: (event) => setState(
+                () {
+                  ref.read(timerProvider)?.cancel();
+                  ref.read(showVideoControlProvider.notifier).update((state) => true);
+                },
+              ),
+              onExit: (event) => setState(
+                () {
+                  ref.read(timerProvider)?.cancel();
+                  ref.read(showVideoControlProvider.notifier).update((state) => true);
+                },
+              ),
+              child: Listener(
+                onPointerDown: (event) {
+                  windowManager.startDragging();
+                },
+                onPointerSignal: (pointerSignal) {
+                  if (pointerSignal is PointerScrollEvent) {
+                    // do something when scrolled
+                    double currentVolume = widget.player.state.volume;
+                    if (pointerSignal.scrollDelta.dy < 0) {
+                      if (currentVolume + 2.0 > 100) {
+                        widget.player.setVolume(100);
+                      } else {
+                        widget.player.setVolume(currentVolume + 2.0);
+                      }
+                    } else {
+                      if (currentVolume - 2.0 < 0) {
+                        widget.player.setVolume(0);
+                      } else {
+                        widget.player.setVolume(currentVolume - 2.0);
+                      }
+                    }
+                    ref.read(volumeStateProvider.notifier).update((state) => widget.player.state.volume);
+                    ref.read(showVolumeProvider.notifier).update((state) => true);
+                  }
+                },
+                onPointerHover: (event) {
+                  ref.read(showVideoControlProvider.notifier).update((state) => true);
+                  setState(() {});
+                  ref.watch(timerProvider)?.cancel();
+                  ref.read(timerProvider.notifier).update(
+                        (state) => Timer(
+                          ref.read(durationProvider),
+                          () {
+                            ref.read(showVideoControlProvider.notifier).update((state) => false);
+                            setState(() {});
+                          },
+                        ),
+                      );
+                },
+                child: GestureDetector(
+                  onDoubleTap: () {
+                    widget.state.toggleFullscreen();
                   },
+                  onSecondaryTapDown: (event) => widget.player.state.playing ? widget.player.pause() : widget.player.play(),
+                  child: Container(
+                    color: Colors.transparent,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                  ),
                 ),
-              );
-        },
-        child: GestureDetector(
-          onDoubleTap: () {
-            widget.state.toggleFullscreen();
-          },
-          onSecondaryTapDown: (event) => widget.player.state.playing ? widget.player.pause() : widget.player.play(),
-          child: CallbackShortcuts(
-            bindings: getShortcuts(widget.state, context, ref, widget.player),
-            child: Container(
-              color: Colors.transparent,
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 85,
-                    left: 30,
-                    child: AnimatedOpacity(
-                      onEnd: () {
-                        ref.read(videoTimerProvider)?.cancel();
-                        ref.read(videoTimerProvider.notifier).state = Timer(_volumeDuration, () {
-                          ref.read(showVolumeProvider.notifier).update((state) => false);
-                          setState(() {});
-                        });
-                      },
-                      opacity: ref.watch(showVolumeProvider) ? 1 : 0,
-                      curve: Curves.decelerate,
-                      duration: const Duration(
-                        milliseconds: 180,
-                      ),
-                      child: Container(
-                        width: 140,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            12,
-                          ),
-                          color: const Color.fromARGB(237, 238, 238, 238),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Volume: ${ref.watch(volumeStateProvider).floorToDouble().round()}",
-                              style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 18, fontWeight: FontWeight.w600),
-                            ),
-                            SizedBox(
-                              width: 115,
-                              child: LinearProgressIndicator(
-                                value: widget.player.state.volume / 100,
-                                color: Colors.blue,
-                              ).pt(5),
-                            ),
-                          ],
-                        ).pltrb(12, 8, 12, 12),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: ref.read(titleBarHeight).toDouble(),
-                    left: 10,
-                    child: AnimatedOpacity(
-                      opacity: ref.watch(showVideoControlProvider) ? 1 : 0,
-                      curve: Curves.decelerate,
-                      duration: const Duration(
-                        milliseconds: 180,
-                      ),
-                      child: MouseRegion(
-                        onEnter: (event) => setState(
-                          () {
-                            ref.read(timerProvider)?.cancel();
-                            ref.read(showVideoControlProvider.notifier).update((state) => true);
-                          },
-                        ),
-                        child: IconButton(
-                          onPressed: () async {
-                            if (widget.state.isFullscreen()) {
-                              await widget.state.toggleFullscreen();
-                            }
-                            if (!context.mounted) return;
-                            Navigator.of(context).pop();
-                          },
-                          icon: const Icon(
-                            Icons.arrow_back_ios_new_rounded,
-                            size: 38,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 20,
-                    child: StreamBuilder<List<String>>(
-                        stream: widget.player.stream.subtitle,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            for (var element in snapshot.data ?? []) {
-                              return Stack(
-                                children: [
-                                  // Text(
-                                  //   element,
-                                  //   style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                  //         // fontWeight: FontWeight.bold,
-                                  //         fontSize: 34,
-                                  //       ),
-                                  // ),
-                                  Text(
-                                    element,
-                                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                          color: Colors.yellow,
-                                          fontSize: 34,
-                                        ),
-                                  ),
-                                ],
-                              );
-                            }
-                          }
-                          return Container();
-                        }),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: AnimatedOpacity(
-                      opacity: ref.watch(showVideoControlProvider) ? 1 : 0,
-                      curve: Curves.decelerate,
-                      duration: const Duration(
-                        milliseconds: 180,
-                      ),
-                      child: MouseRegion(
-                        onEnter: (event) => setState(
-                          () {
-                            ref.read(timerProvider)?.cancel();
-                            ref.read(showVideoControlProvider.notifier).update((state) => true);
-                          },
-                        ),
-                        child: _ControlsOverlay(
-                          videoPlayerSize: widget.player.state.width?.toDouble() ?? 0,
-                          player: widget.player,
-                        ),
-                      ),
-                    ).pb(20),
-                  ),
-                ],
               ),
             ),
-          ),
+            Positioned(
+              top: 85,
+              left: 30,
+              child: AnimatedOpacity(
+                onEnd: () {
+                  ref.read(videoTimerProvider)?.cancel();
+                  ref.read(videoTimerProvider.notifier).state = Timer(_volumeDuration, () {
+                    ref.read(showVolumeProvider.notifier).update((state) => false);
+                    setState(() {});
+                  });
+                },
+                opacity: ref.watch(showVolumeProvider) ? 1 : 0,
+                curve: Curves.decelerate,
+                duration: const Duration(
+                  milliseconds: 180,
+                ),
+                child: Container(
+                  width: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      12,
+                    ),
+                    color: const Color.fromARGB(237, 238, 238, 238),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Volume: ${ref.watch(volumeStateProvider).floorToDouble().round()}",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        width: 115,
+                        child: LinearProgressIndicator(
+                          value: widget.player.state.volume / 100,
+                          color: Colors.blue,
+                        ).pt(5),
+                      ),
+                    ],
+                  ).pltrb(12, 8, 12, 12),
+                ),
+              ),
+            ),
+            Positioned(
+              top: ref.read(titleBarHeight).toDouble(),
+              left: 10,
+              child: AnimatedOpacity(
+                opacity: ref.watch(showVideoControlProvider) ? 1 : 0,
+                curve: Curves.decelerate,
+                duration: const Duration(
+                  milliseconds: 180,
+                ),
+                child: IconButton(
+                  onPressed: () async {
+                    if (widget.state.isFullscreen()) {
+                      await widget.state.toggleFullscreen();
+                    }
+                    if (!context.mounted) return;
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    size: 38,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              child: StreamBuilder<List<String>>(
+                  stream: widget.player.stream.subtitle,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      for (var element in snapshot.data ?? []) {
+                        return Text(
+                          element,
+                          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                color: Colors.yellow,
+                                fontSize: 34,
+                              ),
+                        );
+                      }
+                    }
+                    return Container();
+                  }),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: AnimatedOpacity(
+                opacity: ref.watch(showVideoControlProvider) ? 1 : 0,
+                curve: Curves.decelerate,
+                duration: const Duration(
+                  milliseconds: 180,
+                ),
+                child: _ControlsOverlay(
+                  videoPlayerSize: widget.player.state.width?.toDouble() ?? 0,
+                  player: widget.player,
+                ),
+              ).pb(20),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class _ControlsOverlay extends StatelessWidget {
+class _ControlsOverlay extends ConsumerWidget {
   final Player player;
   final double videoPlayerSize;
 
   const _ControlsOverlay({required this.player, required this.videoPlayerSize});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     const double controlsOverlaySize = 400;
 
     return StreamBuilder<Duration>(
@@ -315,9 +311,19 @@ class _ControlsOverlay extends StatelessWidget {
                         top: 0,
                         right: 0,
                         child: IconButton(
-                          onPressed: () {
-                            // open end drawer
-                            Scaffold.of(context).openEndDrawer();
+                          onPressed: () async {
+                            showDrawer(
+                              context,
+                              builder: (context) => EndDrawerWidget(player: player),
+                              config: const DrawerConfig(
+                                widthPercentage: 0.15,
+                                maxDragExtent: 120,
+                                closeOnClickOutside: true,
+                                closeOnEscapeKey: true,
+                                backdropOpacity: 0.5,
+                                borderRadius: 0,
+                              ),
+                            );
                           },
                           icon: const Icon(
                             Icons.playlist_play_rounded,
@@ -404,4 +410,20 @@ Map<ShortcutActivator, VoidCallback> getShortcuts(VideoState state, BuildContext
       await player.next();
     }
   };
+}
+
+void showVideo(WidgetRef ref, {bool callTimer = true}) {
+  ref.read(showVideoControlProvider.notifier).update((state) => true);
+  ref.watch(timerProvider)?.cancel();
+
+  if (callTimer == true) {
+    ref.read(timerProvider.notifier).update(
+          (state) => Timer(
+            ref.read(durationProvider),
+            () {
+              ref.read(showVideoControlProvider.notifier).update((state) => false);
+            },
+          ),
+        );
+  }
 }
